705a9f9a1e73be6fe5ba0aaba938fd93412ec5a5
nadafakas581-debug <nadafakas581@gmail.com>
2026-05-12 10:45:14 +0100

    Add models for Question, Choice, and Submission

---
+++ models.py
@@ -0,0 +1,26 @@
+from django.db import migrations, models
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    # Each Choice is linked to one Question (1:N Relationship)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+class Submission(models.Model):
+    # Records which question was answered and which choice was selected
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    selected_choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Submission for {self.question.question_text}"
